d3d354b7af5489c6af9d1758b2bf3013ea026103
Luke Parker <lukeparker5132@gmail.com>
2026-04-21 19:42:13 -0400

    Add debug assertion for invariant of `invert`'s `step` algorithm

--- crypto/helioselene/src/field.rs
+++ crypto/helioselene/src/field.rs
@@ -619,6 +619,12 @@ impl Field for HelioseleneField {
       a.as_limbs_mut()[U256::LIMBS - 1] >>= 1;
 
       *u = u.shr_vartime(1);
+
+      debug_assert!(bool::from({
+        #[allow(clippy::as_conversions, clippy::cast_possible_truncation)]
+        let new_a_b_bits = (a.bits() as u32) + (b.bits() as u32);
+        (new_a_b_bits.ct_lt(&a_b_bits)) | a.ct_eq(&U256::ZERO)
+      }));
     }
 
     // Note the limbs still in use so we don't apply operations over unused limbs
@@ -853,4 +859,11 @@ mod tests_assuming_64_bits {
 #[test]
 fn test_helioselene_field() {
   ff_group_tests::prime_field::test_prime_field_bits::<_, HelioseleneField>(&mut rand_core::OsRng);
+}
+
+#[test]
+fn invert_3_66() {
+  let three_66 =
+    HelioseleneField::from_repr(U256::from(3u8).shl_vartime(66).to_le_bytes()).unwrap();
+  assert_eq!(three_66.invert().unwrap() * three_66, HelioseleneField::ONE);
 }
